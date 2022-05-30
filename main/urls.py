283913ef512049e20943814a06c663fccfcc03aa
Mohit Rajbhar <mohitrajbhar49@gmaail.com>
2022-05-30 19:31:54 +0530

    View,Update and Delete added

--- main/urls.py
+++ main/urls.py
@@ -1,14 +1,23 @@
 from django.urls import path,include
-from rest_framework import routers
+# from rest_framework import routers
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
-router = routers.DefaultRouter()
-router.register('User', views.UserView)
-router.register('Tags', views.TagsView)
-router.register('Blogs', views.BlogsView)
+# router = routers.DefaultRouter()
+# router.register('User', views.UserView)
+# router.register('Tag', views.TagsView)
+# router.register('Blogs', views.BlogsView)
 
 urlpatterns = [
-    path('api-root/', include(router.urls)),
+    # path('api-root/', include(router.urls)),
+    path('api-root/blog/', views.BlogsView,),
+    path('api-root/tag/', views.TagsView),
+    path('api-root/user/', views.UserView),
     path('blog/',views.Blog, name='Blog'),
     path('form/',views.Form, name='Form'),
+    path('blog/<int:pk>',views.View_blog, name='View_blog'),
+    path('blog/update/<int:pk>',views.Update, name='Update'),
+    path('blog/delete/<int:pk>',views.Delete, name='Delete'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- main/views.py
+++ main/views.py
@@ -1,35 +1,93 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from rest_framework import viewsets
+from django.shortcuts import render, redirect
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import *
 from .serializers import blogDataSerializer, tagSerializer, userSerializer
 from .forms import BlogForm
 from django.views import View
 
-class BlogsView(viewsets.ModelViewSet):
-    queryset = Blogs.objects.all()
-    serializer_class = blogDataSerializer
+@api_view(['GET', 'POST'])
+def BlogsView(request):
+    if request.method == 'GET':
+        blogs = Blogs.objects.all()
+        serializer = blogDataSerializer(blogs,many= "Ture")
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = blogDataSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class TagsView(viewsets.ModelViewSet):
-    queryset = Tags.objects.all()
-    serializer_class = tagSerializer
+@api_view(['GET', 'POST'])
+def TagsView(request):
+    if request.method == 'GET':
+        tags = Tags.objects.all()
+        serializer = tagSerializer(tags,many= "Ture")
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = tagSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET', 'POST'])
+def UserView(request):
+    if request.method == 'GET':
+        user = User.objects.all()
+        serializer = userSerializer(user,many= "Ture")
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = userSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class UserView(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = userSerializer
 
 def Blog(request):
     content = Blogs.objects.all()
     info = {'content' : content}
     return render(request,"blog.html",info)
 
+
 def Form(request):
     if request.method == "POST":
         formdata = BlogForm(request.POST, request.FILES)
         if formdata.is_valid():
             formdata.save()
+            return redirect("/blog")
 
-    formdata = BlogForm()
-    content = {'formdata':formdata}
-    return render(request,"add_new.html",content)
+    content = BlogForm()
+    info = {'content':content}
+    return render(request,"add_new.html",info)
 
+
+def View_blog(request,pk):
+    content = Blogs.objects.get(pk= pk)
+    info = {'content' : content }
+    return render(request,"view_blog.html",info)
+
+
+def Update(request,pk=0):
+    if request.method == "POST":
+        particularBlog = Blogs.objects.get(pk= pk)
+        update = BlogForm(request.POST, request.FILES ,instance = particularBlog)
+        if update.is_valid():
+            update.save()
+            return redirect("/blog")
+        
+    particularBlog = Blogs.objects.get(pk= pk)
+    content = BlogForm(instance=particularBlog)
+    info = {'content':content}
+    return render(request,"update.html",info)
+
+def Delete(request,pk):
+    particularBlog = Blogs.objects.get(pk= pk)
+    particularBlog.delete()
+    return redirect("/blog")
